2229939345efb9b80dc37918041b62a4a8723b26
Mayank <166874794+Mayank7371@users.noreply.github.com>
2025-09-16 22:54:52 +0530

    Update main.py

--- tokenization/main.py
+++ tokenization/main.py
@@ -1,14 +1,11 @@
 import tiktoken
 
-# Encoder for GPT-4o model
 encoder = tiktoken.encoding_for_model("gpt-4o")
 
 text = "Hey there, my name mayank."
 
-# Tokenization
 tokens = encoder.encode(text)
 print("Tokens:", tokens)
 
-# Detokenization
 decoded = encoder.decode(tokens)
 print("Decoded:", decoded)
